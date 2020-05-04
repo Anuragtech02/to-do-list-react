6a691f362da1f1a6e2518d68e04869548b22426c
Anurag Pal <apal895@gmail.com>
2020-05-04 16:48:14 +0530

    Updates

---
+++ precache-manifest.951f02ea2669702a456a554a7949ac9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c16c32574bce85414d4124170c6ca44",
+    "url": "/to-do-list-react/index.html"
+  },
+  {
+    "revision": "bbb753cedcbee2455ed0",
+    "url": "/to-do-list-react/static/css/main.fc719d83.chunk.css"
+  },
+  {
+    "revision": "c8cd4af9d7862442085c",
+    "url": "/to-do-list-react/static/js/2.f019426e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/to-do-list-react/static/js/2.f019426e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb753cedcbee2455ed0",
+    "url": "/to-do-list-react/static/js/main.35eed012.chunk.js"
+  },
+  {
+    "revision": "e33b5fc751d41a5239b7",
+    "url": "/to-do-list-react/static/js/runtime-main.6237aa1a.js"
+  }
+]);
